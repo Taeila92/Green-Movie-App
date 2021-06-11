529aecd22c8189b3c1a7df236030bd783f94090f
Taeila92 <kim920125@naver.com>
2021-06-11 17:28:19 +0900

    netlify functions update

--- functions/movie.js
+++ functions/movie.js
@@ -1,18 +1,38 @@
-var axios = require('axios');
+const axios = require('axios');
+const {Client_Id, Client_Secret } = process.env
 
-var config = {
-  method: 'get',
-  url: 'https://openapi.naver.com/v1/search/movie.json?query=당신',
-  headers: { 
-    'X-Naver-Client-Id': 'RIbrUrg1MfKCalUPyP3c', 
-    'X-Naver-Client-Secret': 'POBE1FQli9'
+
+exports.handler = async function (event) {
+  console.log(event)
+  const payload = JSON.parse(event.body)
+  const { title } = payload
+  let url = `https://openapi.naver.com/v1/search/movie.json?query=${title}`
+
+  let config = {
+    method: 'get',
+    url,
+    headers: { 
+      'X-Naver-Client-Id': Client_Id, 
+      'X-Naver-Client-Secret': Client_Secret
+    }
   }
-};
-
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+  try {
+    const num = await axios.get(config)
+    return{
+      statusCode : 200,
+      body : JSON.stringify(num.data)
+    }
+  }catch(error) {
+    return {
+      statusCode : error,
+      body: error
+    }
+  }
+//   axios(config)
+//   .then(function (response) {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+// })
+}
